ee4e7dfcba3ec3e9bf6de013ab62d3f64c7e0cd3
Fotsingboris <fotsingtchoupe1@gmail.com>
2024-09-14 17:53:22 +0100

    fix: removed informations on teh nave abd header

--- simpleinvoice/settings.py
+++ simpleinvoice/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                 'download.context_processors.company_info',
             ],
         },
     },
